178989d8359085b1494c10a5a5740caba0a7e686
nordin001 <73000909+nordin001@users.noreply.github.com>
2023-12-08 21:12:21 +0100

    first post
    falla el post

--- src/index.js
+++ src/index.js
@@ -7,26 +7,47 @@ const mysql = require("mysql");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Password1234",
+  password: "",
   database: "entrenamientos"
 })
-
 
 connection.connect((err) => {
   if (err) {
     throw err
   }
+  console.log("correcto")
 })
 
 const PORT = process.env.PORT || 3000;
 
-
 app.use("/api/v1/workouts", routerWorkouts)
 app.use("/api/v1/users", routerUsers)
+
+// Middleware para parsear el cuerpo de las solicitudes en formato JSON
+app.use(express.json());
+
+
+
+
 
 
 app.listen(PORT, () => {
   console.log(`listening ${PORT}`);
+});
+
+app.post('/insertar', (req, res) => {
+  const { nombre, passworld } = { nombre: "user", passworld: "123" };
+
+  // Realizar la inserción en la base de datos
+  const sql = 'INSERT INTO tu_tabla (nombre, passworld) VALUES (?, ?)';
+  connection.query(sql, [nombre, passworld], (error, results, fields) => {
+    if (error) {
+      console.error('Error al insertar en la base de datos:', error);
+      res.status(500).json({ error: 'Error interno del servidor' });
+      return;
+    }
+    res.status(200).json({ mensaje: 'Registro insertado correctamente' });
+  });
 });
 
 module.exports = connection
